dac39ae3fa69bbf8f8eb9a6a995e68ee41584e17
Christabelle Halim <chritabellesawo15@gmail.com>
2026-03-27 15:42:55 +0800

    hehe

---
+++ q1/q1.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+
+class Pair {
+    String name;
+    String month;
+    Pair(String name, String month) {
+            this.name = name;
+            this.month = month;
+        }
+        int getbulan(HashMap<String, Integer> monthMap) {
+            return 0;
+        }
+    }
+
+    class Pair2 extends Pair {
+        Pair2(String name, String month){
+        super (name, month);}
+
+        @Override 
+        int getbulan(HashMap<String, Integer> monthMap) {
+            return monthMap.get(month.toLowerCase());
+    }
+}
+    public class q1 {
+    public static List<String> solve(String input) {
+       HashMap<String, Integer> monthMap = new HashMap<>();
+        monthMap.put("january", 1);
+        monthMap.put("february", 2);
+        monthMap.put("march", 3);
+        monthMap.put("april", 4);
+        monthMap.put("may", 5);
+        monthMap.put("june", 6);
+        monthMap.put("july", 7);
+        monthMap.put("august", 8);
+        monthMap.put("september", 9);
+        monthMap.put("october", 10);
+        monthMap.put("november", 11);
+        monthMap.put("december", 12);
+      
+        Stack<String> stack = new Stack<>();
+        String [] kata = input.split(" ");
+        for (String word : kata) {
+            stack.push(word);
+        }
+
+        List<Pair> pair = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            String dua = stack.pop();
+            String satu = stack.pop();
+            pair.add(new Pair2 (satu, dua)); 
+        }
+
+        Collections.sort(pair, (a, b) -> {
+            int indexA = a.getbulan(monthMap);
+            int indexB = b.getbulan(monthMap);
+        return Integer.compare(indexA, indexB);
+        } );
+        
+        Stack<String> output = new Stack<>();
+        for (Pair perbandingan : pair) {
+            output.push(perbandingan.name);
+            output.push(perbandingan.month);
+        }
+        return new ArrayList<>(output);
+    }
+
+    public static void main(String[] args) {
+        String input = "holly may interesting MARCH corey November junior january paul december";
+        System.out.println(solve(input));
+    }
+
+}
---
+++ q2/q2.java
@@ -0,0 +1,104 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+abstract class Stand {
+    abstract boolean exposed(String name);
+}
+
+class Josuke extends Stand {
+    public boolean exposed(String name) {
+        return name.toLowerCase().startsWith("k");
+    }
+}
+
+class Jotaro extends Stand {
+    public boolean exposed(String name) {
+    return name.length() <= 3; 
+    }
+}
+
+class Okuyasu extends Stand {
+    public boolean exposed(String name) {
+        name = name.toLowerCase();
+        for (int i = 0; i < name.length() - 1; i++){
+            if (name.charAt(i) == name.charAt(i + 1)){
+                return true; } 
+        }
+        return false;
+        }
+    }
+
+class Koichi extends Stand {
+    public boolean exposed(String name) {
+    String vowel = "aiueo";
+    int count = 0;
+    for (char c : name.toLowerCase().toCharArray()){
+        if (vowel.indexOf(c) != -1){
+            count++;}
+    }
+    return count >= 3;
+    }
+}
+
+class Rohan extends Stand {
+    public boolean exposed(String name) {
+    name = name.toLowerCase();
+    String reverse = new StringBuilder(name).reverse().toString();
+    return name.equals(reverse);
+    }
+}
+
+public class q2 {
+    public static Stand getStand(String name) {
+        switch (name){
+            case "Josuke":
+                return new Josuke();
+            case "Jotaro":
+                return new Jotaro();
+            case "Okuyasu":
+                return new Okuyasu();
+            case "Koichi":
+                return new Koichi();
+            case "Rohan":
+                return new Rohan();
+            default: 
+                return null;
+        }
+    }
+
+    public static void check(String standName, String input) {
+        Queue<String> queue = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+        String[] names = input.trim().split("\\s+"); 
+        for(String name : names) {
+            queue.add(name);
+        }
+        Stand stand = getStand(standName);
+        while (!queue.isEmpty()) {
+            String name = queue.poll();
+            if(stand.exposed(name)) {
+                stack.push(name.toLowerCase());
+            }
+        }
+        if (!stack.isEmpty()) {
+            System.out.println(standName + " exposed someone!");
+        }
+        else {
+            System.out.println(standName + " exposed no one");
+        }
+
+        System.out.println("Arrested: " + stack);
+    }
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String standName = sc.nextLine();
+    String names = sc.nextLine();
+    check (standName, names);
+
+    sc.close();
+    }
+}
+
